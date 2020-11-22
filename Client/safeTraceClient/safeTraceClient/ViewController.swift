3d9d6d815541d3d480fe0d5cd083bc3a8efdbd44
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2020-11-22 17:16:03 +0000

    Commenced work on the iOS Client

---
+++ Client/safeTraceClient/safeTraceClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  safeTraceClient
+//
+//  Created by Razvan-Antonio Berbece on 22/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
